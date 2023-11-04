99c7b3f8db025201cd6a316b852504f476b273b6
jorgelopezd <jorge_lopezd@hotmail.com>
2023-11-04 11:18:47 -0300

    se suben cambios de diseño

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import IconHeart from './IconHeart';
 
 const Gallery = ({ fotos }) => {
-  const handleLike = (id) => {
-
-  }
   return (
     <div className="gallery grid-columns-5 p-3">
       {fotos.map((foto) => (
-        <div key={foto.id} className="card" >
-          <img className="card-img" src={foto.keyImage} alt={foto.title} />
-          <div className="card-body">
-            <h5 className="card-title">{foto.title}</h5>
-            <IconHeart filled={foto.liked} onClick={() => handleLike(foto.id)}/>
+        <div key={foto.id} className="card">
+          <div className='img-wrapper'>
+            <img className="card-img" src={foto.keyImage} alt={foto.title} />
           </div>
+          <div className="card-body">
+            <h3 className="card-title">{foto.title}</h3>
+          </div>
+            <div className='iconheart'>
+                <IconHeart filled={foto.liked} onClick={() => handleLike(foto.id)} />
+            </div>
         </div>
       ))}
     </div>
--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -1,9 +1,22 @@
+import { useContext } from 'react';
+import ContextFoto from '../context/my_context';
+
 const Favorites = () => {
   return (
     <div>
       <h1>Fotos favoritas</h1>
-      <div className="p-3 gallery grid-columns-4"></div>
+      <div className="p-3 gallery grid-columns-4">
+        {favoritePhotos.map((foto) => (
+          <div key={foto.id} className="card">
+            <img className="card-img" src={foto.keyImage} alt={foto.title} />
+            <div className="card-body" onClick={() => toggleLiked(foto.id)}>
+              <h5 className="card-title">{foto.title}</h5>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
+
 export default Favorites;
